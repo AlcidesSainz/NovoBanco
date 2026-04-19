098cc75b19cd09631b34c81ff1da7342d4834908
Sainz <Sainz@DESKTOP-H8N27FS>
2026-04-19 11:15:01 -0500

    El campo referencia para las transferencias, depositos y retiros fue convertido en un guid para que se genere automaticamente

--- NovoBanco.Application/Dtos/Transactions/TransferRequestDto.cs
+++ NovoBanco.Application/Dtos/Transactions/TransferRequestDto.cs
@@ -7,5 +7,4 @@ public class TransferRequestDto
     public Guid SourceAccountId { get; set; }
     public Guid DestinationAccountId { get; set; }
     public decimal Amount { get; set; }
-    public string Reference { get; set; } = string.Empty;
 }
